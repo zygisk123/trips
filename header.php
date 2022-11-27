53b71809c777bf5be81c11a12a8f6c8ddf65fa4e
zygisk123 <zygimantask13@gmail.com>
2022-11-27 18:24:37 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * This is the template that displays all of the <head> section and everything up until <div id="content">
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ *
+ * @package trips
+ */
+
+?>
+<!doctype html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="profile" href="https://gmpg.org/xfn/11">
+	<?php wp_head(); ?>
+	<!-- CSS only -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+	<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+</head>
+
+<body <?php body_class(); ?>>
+<?php wp_body_open(); ?>
+<div id="page" class="site">
+	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'trips' ); ?></a>
+
+	<header id="masthead" class="site-header">
+		<div class="site-branding">
+			<?php
+			the_custom_logo();
+			if ( is_front_page() && is_home() ) :
+				?>
+				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+				<?php
+			else :
+				?>
+				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+				<?php
+			endif;
+			$trips_description = get_bloginfo( 'description', 'display' );
+			if ( $trips_description || is_customize_preview() ) :
+				?>
+				<p class="site-description"><?php echo $trips_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
+			<?php endif; ?>
+		</div><!-- .site-branding -->
+
+	</header><!-- #masthead -->
---
+++ page-trips.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package trips
+ */
+
+get_header();
+?>
+	<?php
+	$query = new WP_QUERY(array(
+		'post_type' => 'trips'
+	));
+	?>
+
+    <main id="primary" class="site-main">
+		<div class="container">
+			<table class="table" >
+				<thead>
+					<tr>
+						<th>Month</th>
+						<th>Distance</th>
+						<th>Max people allowed</th>
+						<th>With Animals</th>
+					</tr>
+				</thead>
+				<tbody>
+					<?php
+						if ($query->have_posts()) {
+							while ($query->have_posts()) {?>
+								<?php $query->the_post(); ?>
+								<div class="item d-inline col-3 mt-3 mb-3">
+									<tr>
+										<td class="tripMonth">
+											<?=get_field('month', get_the_ID())?>
+										</td>
+										<td class="tripDistance">
+											<?=get_field('distance', get_the_ID())?>
+										</td>  
+										<td class="tripPeopleAllowed">
+											<?=get_field('max_people_allowed', get_the_ID())?>
+										</td>  
+										<td class="withAnimals">
+											<?=get_field('with_animals', get_the_ID())?>
+										</td>  
+									</tr>
+								</div>
+						<?php }}
+					?>
+				</tbody>
+			</table>
+		</div>
+    </main><!-- #main -->
+
+<?php
+get_footer();
